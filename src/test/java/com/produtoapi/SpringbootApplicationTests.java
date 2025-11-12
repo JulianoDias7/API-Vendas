a2b99a1a0fe7ada1fecc81b05f1fb9562dc6223c
JulianoDias7 <julianodias1995@gmail.com>
2025-11-12 17:10:25 -0300

    Ajusta a configuração do banco de dados

--- src/test/java/com/produtoapi/SpringbootApplicationTests.java
+++ src/test/java/com/produtoapi/SpringbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicoSpringbootApplicationTests {
+class SpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
